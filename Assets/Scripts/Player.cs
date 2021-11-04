2ba3dc92b684e024ccb0ec084dd9f9bc33fe3477
Suleiman Abdullah <suleimanabdullahdev@gmail.com>
2021-11-04 17:09:01 +0300

    mapping user input with player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float _moveSpeed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //to access the horizontal we need a handle to hold the return horizontal axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime);
     }
+
+
+
+
+
 }
