db3bf7880f6b9d7cb52a1f8db57f19271e02ef70
Suleiman Abdullah <suleimanabdullahdev@gmail.com>
2021-11-07 15:32:02 +0300

    Aplly Object oreinted programming to Player Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,44 +15,36 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        CalculateMovement();   
+    }
+
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         transform.Translate(Vector3.right * horizontalInput * _moveSpeed * Time.deltaTime);
         transform.Translate(Vector3.up * verticalInput * _moveSpeed * Time.deltaTime);
 
-        //if player position on the y is > than 0 reset y pos = 0
-        //else if position on the y is < than -3.9f y pos = -3.9f
-
-
-        if(transform.position.y >=0)
+        if (transform.position.y >= 0)
         {
             transform.position = new Vector3(transform.position.x, 0, 0);
         }
-        else if(transform.position.y <= -3.9f)
+        else if (transform.position.y <= -3.9f)
         {
-            transform.position = new Vector3(transform.position.x,-3.9f, 0);
+            transform.position = new Vector3(transform.position.x, -3.9f, 0);
         }
 
-        // if player on the x > 11
-        // x pos = -11
-        // else if player < -11
-        // x pos = 11
-        if(transform.position.x > 11)
+        if (transform.position.x > 11.5f)
         {
-            transform.position = new Vector3(-11, transform.position.y, 0);
+            transform.position = new Vector3(-11.5f, transform.position.y, 0);
         }
 
-        else if (transform.position.x < -11)
+        else if (transform.position.x < -11.5f)
         {
-            transform.position = new Vector3(11, transform.position.y, 0);
+            transform.position = new Vector3(11.5f, transform.position.y, 0);
         }
-        
-
     }
-
-
-
-
 
 }
