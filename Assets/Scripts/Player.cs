b5bc67695814ce946f4829898102282004f35185
Suleiman Abdullah <suleimanabdullahdev@gmail.com>
2022-01-09 15:10:04 +0300

    Implement lives Display

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -103,6 +103,7 @@ public class Player : MonoBehaviour
             return;
         }
         _lives--;
+        _uiManager.UpdateLives(_lives);
 
         if (_lives < 1)
         {
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,15 +8,23 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField]
     private Text _scoreText;
-    
+    [SerializeField]
+    private Image _imageLives;
+    [SerializeField]
+    private Sprite[] _livesSprite;
+
     void Start()
     {
         _scoreText.text = "Score:" + 0;
     }
-    
+
     public void UpdateScore(int score)
     {
         _scoreText.text = "Score:" + score;
     }
 
+    public void UpdateLives(int currentLives)
+    {
+        _imageLives.sprite = _livesSprite[currentLives];
+    }
 }
